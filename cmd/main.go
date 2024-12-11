1864c9a636471121e32b998afe299d876350e5f5
mickey-mickser <msunavong@gmail.com>
2024-12-11 14:55:18 +0200

    updatee

---
+++ cmd/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq" // _ это для инициализации драйвера
+	"github.com/mickey_mickser/gogo"
+	"github.com/mickey_mickser/gogo/pkg/handler"
+	"github.com/mickey_mickser/gogo/pkg/repository"
+	"github.com/mickey_mickser/gogo/pkg/service"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	if err := InitConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		logrus.Fatalf("error initializing DB: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running cd http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("ToDo Down")
+	///
+
+	//if err := srv.Shutdown(context.Background()); err != nil {
+	//	logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	//}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logrus.Errorf("error occurred on server shutdown: %s", err.Error())
+	}
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
+	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
